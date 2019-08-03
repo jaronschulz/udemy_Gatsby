2bdb975b991bc244f973090c1b6d4f2b029925e5
Jaron Schulz <jaronchulz@gmail.com>
2019-08-03 14:36:50 +0200

    installed styled components and created a button comp

--- 0.IntroProject/src/pages/index.js
+++ 0.IntroProject/src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import { Button } from '../components/Button'
 
 export default () => (
   <Layout>
     <h1>this is our Home Page</h1>
+    <Button>styled button</Button>
   </Layout>
 )
