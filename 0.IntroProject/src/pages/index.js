4a4abaf1b9a9ce4ff49000920e20417fabde6d08
Jaron Schulz <jaronchulz@gmail.com>
2019-08-03 12:24:24 +0200

    comparing a tag and Linkcomponent

--- 0.IntroProject/src/pages/index.js
+++ 0.IntroProject/src/pages/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 export default () => (
   <div>
     <h1>this is our Home Page</h1>
+    <div>
+      {/* <a href="/blog">blog</a> */}
+      <Link to="/blog">blog</Link>
+    </div>
+    <div>
+      <a href="https://www.gatsby.org">gatsby page</a>
+    </div>
   </div>
 )
