e37999d84b30a4d6fdf020f5660dba8de9d99c17
Jaron Schulz <jaronchulz@gmail.com>
2019-08-03 12:56:08 +0200

    created Navbar component with links

--- 0.IntroProject/src/pages/blog.js
+++ 0.IntroProject/src/pages/blog.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 const Blog = () => {
   return (
     <div>
       <h1>this is our Blog Page</h1>
-      <div>
-        {/* <a href="/">home page</a> */}
-        <Link to="/">home page</Link>
-      </div>
     </div>
   )
 }
