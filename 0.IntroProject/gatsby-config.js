01e9533445cf39a866f419dd76229892d9b00009
Jaron Schulz <jaronchulz@gmail.com>
2019-08-07 15:12:51 +0200

    env variables added and installed contetnful plugin

--- 0.IntroProject/gatsby-config.js
+++ 0.IntroProject/gatsby-config.js
@@ -4,6 +4,9 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 const path = require('path');
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   siteMetadata: {
@@ -27,5 +30,14 @@ module.exports = {
         path: path.join(__dirname, `src`, `images`),
       },
     },
-  ],
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        // accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],‚
 };
