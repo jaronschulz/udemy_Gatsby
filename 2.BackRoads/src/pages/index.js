33b92345be3b2902e3864dea6385cfbbd3af0a9b
Jaron Schulz <jaronchulz@gmail.com>
2019-08-25 15:22:54 +0200

    created a hero component with a banner component for HOME/index

--- 2.BackRoads/src/pages/index.js
+++ 2.BackRoads/src/pages/index.js
@@ -1,15 +1,27 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
+import Layout from '../components/layout'
+import SimpleHero from '../components/simple-hero'
+import Banner from '../components/banner'
 
 const home = () => {
-  return (
-    <Layout>
-      Hello World!
-      <Link to="/blog/">Blog</Link>
-    </Layout>
-  )
+    return (
+        <Layout>
+            <SimpleHero>
+                <Banner
+                    title="continue exploring"
+                    info="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
+                recusandae aperiam a"
+                >
+                    <Link to="/tours/" className="btn-white">
+                        explore tours
+                    </Link>
+                </Banner>
+            </SimpleHero>
+            <h2>Hello from Home!!!!!!!!!!!!!!</h2>
+        </Layout>
+    )
 }
 
 export default home
