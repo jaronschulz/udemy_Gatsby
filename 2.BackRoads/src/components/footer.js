a4a7cfa6a438f69898e1f4e17bf2c1b822c81cc4
Jaron Schulz <jaronchulz@gmail.com>
2019-08-25 15:00:23 +0200

    created footer similar to navbar with maping through the array

--- 2.BackRoads/src/components/footer.js
+++ 2.BackRoads/src/components/footer.js
@@ -1,11 +1,39 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
+import { FaRProject } from 'react-icons/fa'
+
+import styles from '../css/footer.module.css'
+import links from '../constants/links'
+import socialIcons from '../constants/social-icons'
 
 const Footer = () => {
-  return (
-    <div>
-      <h1>FOOTER HERE !!!!!</h1>
-    </div>
-  )
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.links}>
+                {links.map((link, i) => (
+                    <Link key={i} to={link.path}>
+                        {link.text}
+                    </Link>
+                ))}
+            </div>
+            <div className={styles.icons}>
+                {socialIcons.map((item, i) => (
+                    <a
+                        key={i}
+                        href={item.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {item.icon}
+                    </a>
+                ))}
+            </div>
+            <div className={styles.copyright}>
+                copyright &copy; backroads travel company{' '}
+                {new Date().getFullYear()} all
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
