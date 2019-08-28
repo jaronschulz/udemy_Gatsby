cffb3b3493c26c3271a34ba811cc15e74f5d5ddb
Jaron Schulz <jaronchulz@gmail.com>
2019-08-28 12:09:13 +0200

    setting up source-filesystem plugin for images - replaced image import (aboutPage) with graphql query

--- 2.BackRoads/gatsby-config.js
+++ 2.BackRoads/gatsby-config.js
@@ -4,5 +4,16 @@ module.exports = {
         description: `Explore awesome parts of the world & discover which makes ech of them unique. FOrget your daily routine & say yes to adventure!`,
         author: `@johndoe`,
     },
-    plugins: [`gatsby-plugin-styled-components`],
+    plugins: [
+        `gatsby-plugin-styled-components`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images/`,
+            },
+        },
+    ],
 }
--- 2.BackRoads/src/pages/blog.js
+++ 2.BackRoads/src/pages/blog.js
@@ -1,29 +1,7 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const blog = ({
-    data: {
-        site: { siteMetadata },
-    },
-}) => (
-    <Layout>
-        hello from blog page !!
-        <h1>title: {siteMetadata.title}</h1>
-        <h1>author: {siteMetadata.author}</h1>
-    </Layout>
-)
+const blog = () => <Layout>hello from blog page !!</Layout>
 
-export const query = graphql`
-    query {
-        site {
-            siteMetadata {
-                title
-                description
-                author
-            }
-        }
-    }
-`
 export default blog
